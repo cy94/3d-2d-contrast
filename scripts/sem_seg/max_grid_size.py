a7a1d6a5429c26ad6d5c3af9071f71c5696e9be0
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-12 16:39:34 +0200

    Add script to find max grid size

---
+++ scripts/sem_seg/max_grid_size.py
@@ -0,0 +1,39 @@
+'''
+Find the maximum voxel grid size over all scans
+'''
+import argparse
+
+from pathlib import Path
+
+import os
+from tqdm import tqdm
+import numpy as np
+
+import torch
+
+
+def main(args):
+    root = Path(args.scannet_dir)
+
+    grid_sizes = []
+
+    for scan_id in tqdm(sorted(os.listdir(root))[:200], desc='scan'):
+        scan_dir = root / scan_id
+        
+        in_file = f'{scan_id}_occ_grid.pth'
+        data = torch.load(scan_dir / in_file)
+        grid = data['y']
+        grid_sizes.append(grid.shape)
+
+    print(f'Max grid size: {np.array(grid_sizes).max(axis=0)}')
+
+
+if __name__ == '__main__':
+    # params
+    parser = argparse.ArgumentParser()
+    # data paths
+    parser.add_argument('scannet_dir', help='path to scannet root dir file to read')
+
+    args = parser.parse_args()
+
+    main(args)
