35d212d6da9fa0b67f9d39fdd55eb626c41d33ff
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-17 12:06:30 +0200

    Pad grid with constant values

--- transforms/grid_3d.py
+++ transforms/grid_3d.py
@@ -19,8 +19,8 @@ class Pad:
         # left and right padding for 3 dims (ie l/r, front/back, top/bottom)
         pad = np.stack((np.floor(diff/2), np.ceil(diff/2)), axis=-1).astype(np.uint8).tolist()
         
-        new_sample['x'] = np.pad(new_sample['x'], pad)
-        new_sample['y'] = np.pad(new_sample['y'], pad)
+        new_sample['x'] = np.pad(new_sample['x'], pad, constant_values=-100)
+        new_sample['y'] = np.pad(new_sample['y'], pad, constant_values=-100)
 
         return new_sample
 
