2b005dec86848c168897b780f3978fa3ff1616f2
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-17 15:56:14 +0200

    Add 3d FCN with conv transpose

--- models/sem_seg/fcn3d.py
+++ models/sem_seg/fcn3d.py
@@ -15,15 +15,41 @@ class FCN3D(nn.Module):
 
         '''
         super().__init__()
-        self.model = nn.Sequential(
+        self.layers = nn.ModuleList([
             # args: inchannels, outchannels, kernel, stride, padding
-            nn.Conv3d(in_channels, 8, 3, 2),
-            nn.Conv3d(8, 16, 3, 2),
-            nn.Conv3d(16, 32, 3, 2),
-            nn.Conv3d(32, 64, 3, 2),
-            nn.Conv3d(64, num_classes, 3, 2),
-            nn.Upsample(grid_size[::-1])
-        )
+            # 1->1/2
+            nn.Conv3d(in_channels, 32, 3, 2, 1),
+            # same
+            nn.Conv3d(32, 32, 3, 1, 1),
+            nn.Conv3d(32, 64, 3, 1, 1),
+            nn.ReLU(),
+
+            # 1/2->1/4
+            nn.Conv3d(64, 64, 3, 2, 1),
+            # same
+            nn.Conv3d(64, 64, 3, 1, 1),
+            nn.Conv3d(64, 128, 3, 1, 1),
+            nn.ReLU(),
+            
+            # 1.4->1/8
+            nn.Conv3d(128, 128, 3, 2, 1),
+            # same
+            nn.Conv3d(128, 128, 3, 1, 1),
+            nn.Conv3d(128, 256, 3, 1, 1),
+            nn.ReLU(),
+            
+            # inchannels, outchannels, kernel, stride, padding, output_padding
+            # 1/8->1/4
+            nn.ConvTranspose3d(256, 128, 4, 2, 1),
+            nn.ReLU(),
+            # 1/4->1/2
+            nn.ConvTranspose3d(128, 64, 4, 2, 1),
+            nn.ReLU(),
+            # 1/2->original shape
+            nn.ConvTranspose3d(64, num_classes, 4, 2, 1),
+        ])
 
     def forward(self, x):
-        return self.model(x)
+        for layer in self.layers:
+            x = layer(x)
+        return x
