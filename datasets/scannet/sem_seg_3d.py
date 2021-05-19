34768386ea10bd2f4c9a06a312f26c95d658bea7
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-19 12:02:50 +0200

    Sample subvol from grid for training

--- datasets/scannet/sem_seg_3d.py
+++ datasets/scannet/sem_seg_3d.py
@@ -27,30 +27,54 @@ class ScanNetSemSegOccGrid(Dataset):
 
     labels as given here here: http://kaldir.vc.in.tum.de/scannet_benchmark/
     '''
-    def __init__(self, root_dir, limit_scans=None, transform=None):
-        self.root_dir = Path(root_dir)
+    def __init__(self, cfg, transform=None):
+        '''
+        data_cfg:
+            see configs/occgrid_train.yml
+            root: root of scannet dataset
+            limit_scans: read only these many scans
+            subvol_size: size of subvolumes to sample
+            subvols_per_scene: sample these many subvolumes per scene
+            transform: apply on each subvol
+        '''
+        root_dir = Path(cfg['root'])
+        
+        self.subvols_per_scene = cfg['subvols_per_scene']
         self.paths = []
         self.transform = transform
+        self.subvol_size = cfg['subvol_size']
 
-        scans = sorted(os.listdir(self.root_dir))
-        if limit_scans:
-            scans = scans[:limit_scans]
+        scans = sorted(os.listdir(root_dir))
+
+        if cfg['limit_scans']:
+            scans = scans[:cfg['limit_scans']]
 
         for scan_id in scans:
-            path = self.root_dir / scan_id / f'{scan_id}_occ_grid.pth'
+            path = root_dir / scan_id / f'{scan_id}_occ_grid.pth'
 
             if path.exists():
                 self.paths.append(path)
 
     def __len__(self):
-        return len(self.paths)
+        # vols per scene * num scenes
+        return self.subvols_per_scene * len(self.paths)
 
     def __getitem__(self, ndx):
-        path = self.paths[ndx]
+        # pick the scene
+        scene_ndx = ndx // self.subvols_per_scene
+        path = self.paths[scene_ndx]
+        # load the full scene
         data = torch.load(path)
-        x, y_orig = data['x'], data['y']
+        x, y = data['x'], data['y']
+        # pick a random subvolume
+        max_start = np.array(x.shape) - self.subvol_size
+        start = np.random.randint((0, 0, 0), max_start, dtype=np.uint8)
+        end = start + self.subvol_size
 
-        y_cts = nyu40_to_continuous(y_orig).astype(np.int8)
+        x = x[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
+        y = y[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
+
+        y_cts = nyu40_to_continuous(y).astype(np.int8)
 
         sample = {'path': path, 'x': x, 'y': y_cts}
 
