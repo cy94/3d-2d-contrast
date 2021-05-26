10dd38fbd52830834c99e3af3495a26215a9d1fe
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-26 11:09:29 +0200

    Add pt lightining fcn3d model

--- models/sem_seg/fcn3d.py
+++ models/sem_seg/fcn3d.py
@@ -1,20 +1,30 @@
 '''
 3D fully conv network
 '''
+import random
+
+import numpy as np
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class FCN3D(nn.Module):
+import pytorch_lightning as pl
+
+from eval.sem_seg_2d import miou
+
+class FCN3D(pl.LightningModule):
     '''
     Dense 3d convs on a volume grid
     '''
-    def __init__(self, in_channels, num_classes):
+    def __init__(self, in_channels, num_classes, cfg=None):
         '''
         in_channels: number of channels in input
 
         '''
         super().__init__()
+        self.cfg = cfg
+
         self.layers = nn.ModuleList([
             # args: inchannels, outchannels, kernel, stride, padding
             # 1->1/2
@@ -50,3 +60,43 @@ class FCN3D(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def common_step(self, batch, batch_idx):
+        x, y = batch['x'], batch['y']
+        out = self(x)
+        loss = F.cross_entropy(out, y)
+        return out, loss
+
+    def training_step(self, batch, batch_idx):
+        out, loss = self.common_step(batch, batch_idx)
+        self.log('train_loss', loss)
+
+        # miou only for some batches
+        if random.random() > 0.8:
+            m = self.get_miou(batch, out)
+            self.log('miou/train', m)
+
+        return loss
+
+    def get_miou(self, batch, out):
+        preds = out.argmax(dim=1)
+        m = miou(preds, batch['y'], 21)
+        return m if not np.isnan(m).any() else None
+            
+    def validation_step(self, batch, batch_idx):
+        out, loss = self.common_step(batch, batch_idx)
+
+        if random.random() > 0.8:
+            m = self.get_miou(batch, out)
+            self.log('miou/val', m)
+
+        return loss
+    
+    def validation_epoch_end(self, validation_step_outputs):
+        val_loss = torch.Tensor(validation_step_outputs).mean()
+        self.log('val_loss', val_loss)
+        
---
+++ scripts/sem_seg/train_occgrid_lt.py
@@ -0,0 +1,107 @@
+
+import os 
+import argparse
+from datetime import datetime as dt
+import random 
+
+import numpy as np
+
+from tqdm import tqdm
+
+from lib.misc import read_config
+from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid, collate_func
+from transforms.grid_3d import AddChannelDim, Pad, TransposeDims
+from models.sem_seg.utils import count_parameters
+from models.sem_seg.fcn3d import FCN3D
+from eval.sem_seg_2d import miou
+
+from torchinfo import summary
+import torch
+from torch.optim import Adam
+import torch.nn.functional as F
+from torch.utils.data import Subset, DataLoader
+from torchvision.transforms import Compose
+from torch.utils.tensorboard import SummaryWriter
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+def log_metrics(self, metrics, step=None):
+    for k, v in metrics.items():
+        if isinstance(v, dict):
+            self.experiment.add_scalars(k, v, step)
+        else:
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            self.experiment.add_scalar(k, v, step)
+
+
+def monkeypatch_tensorboardlogger(logger):
+    import types
+
+    logger.log_metrics = types.MethodType(log_metrics, logger)
+
+def main(args):
+    cfg = read_config(args.cfg_path)
+
+    # create transforms list
+    transforms = []
+    transforms.append(AddChannelDim())
+    transforms.append(TransposeDims())
+    t = Compose(transforms)
+
+    dataset = ScanNetSemSegOccGrid(cfg['data'], transform=t)
+
+    if cfg['train']['train_split']:
+        train_size = int(cfg['train']['train_split'] * len(dataset))
+        train_set = Subset(dataset, range(train_size))
+        val_set = Subset(dataset, range(train_size, len(dataset)))
+    elif cfg['train']['train_size'] and cfg['train']['val_size']:
+        train_set = Subset(dataset, range(cfg['train']['train_size']))
+        val_set = Subset(dataset, range(cfg['train']['train_size'], 
+                            cfg['train']['train_size']+cfg['train']['val_size']))
+    else:
+        raise ValueError('Train val split not specified')
+
+
+    print(f'Train set: {len(train_set)}')
+    print(f'Val set: {len(val_set)}')
+
+    train_loader = DataLoader(train_set, batch_size=cfg['train']['train_batch_size'],
+                            shuffle=True, num_workers=8, collate_fn=collate_func,
+                            pin_memory=True)  
+
+    val_loader = DataLoader(val_set, batch_size=cfg['train']['val_batch_size'],
+                            shuffle=False, num_workers=8, collate_fn=collate_func,
+                            pin_memory=True) 
+
+    model = FCN3D(in_channels=1, num_classes=21, cfg=cfg)
+    print(f'Num params: {count_parameters(model)}')
+
+    input_size = (cfg['train']['train_batch_size'], 1,) + tuple(cfg['data']['subvol_size'])
+    summary(model, input_size=input_size)
+
+    if cfg['train']['resume']:
+        pass
+    
+    checkpoint_callback = ModelCheckpoint(save_last=True, save_top_k=5, 
+                                        monitor='val_loss')
+
+    trainer = pl.Trainer(gpus=1, 
+                        auto_scale_batch_size='binsearch',
+                        callbacks=[checkpoint_callback],
+                        max_epochs=cfg['train']['epochs'],
+                        val_check_interval=cfg['train']['eval_intv'])
+    monkeypatch_tensorboardlogger(trainer.logger)                        
+    trainer.fit(model, train_loader, val_loader)
+
+    return 
+  
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('cfg_path', help='Path to cfg')
+    parser = pl.Trainer.add_argparse_args(p)
+    args = p.parse_args()
+
+    main(args)
