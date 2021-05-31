99f6ec5c67028210499361b9b10a1139d779f02c
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-31 21:15:45 +0200

    Remove FCN params by reducing channels

--- models/sem_seg/fcn3d.py
+++ models/sem_seg/fcn3d.py
@@ -47,27 +47,27 @@ class FCN3D(pl.LightningModule):
             # 1->1/2
             nn.Conv3d(in_channels, 32, 3, 2, 1),
             # same
-            nn.Conv3d(32, 64, 3, 1, 1),
+            nn.Conv3d(32, 32, 3, 1, 1),
             nn.ReLU(),
 
             # 1/2->1/4
-            nn.Conv3d(64, 64, 3, 2, 1),
+            nn.Conv3d(32, 32, 3, 2, 1),
             # same
-            nn.Conv3d(64, 128, 3, 1, 1),
+            nn.Conv3d(32, 64, 3, 1, 1),
             nn.ReLU(),
             
             # 1.4->1/8
-            nn.Conv3d(128, 128, 3, 2, 1),
+            nn.Conv3d(64, 64, 3, 2, 1),
             # same
-            nn.Conv3d(128, 128, 3, 1, 1),
+            nn.Conv3d(64, 64, 3, 1, 1),
             nn.ReLU(),
             
             # inchannels, outchannels, kernel, stride, padding, output_padding
             # 1/8->1/4
-            nn.ConvTranspose3d(128, 128, 4, 2, 1),
+            nn.ConvTranspose3d(64, 64, 4, 2, 1),
             nn.ReLU(),
             # 1/4->1/2
-            nn.ConvTranspose3d(128, 64, 4, 2, 1),
+            nn.ConvTranspose3d(64, 64, 4, 2, 1),
             nn.ReLU(),
             # 1/2->original shape
             nn.ConvTranspose3d(64, num_classes, 4, 2, 1),
