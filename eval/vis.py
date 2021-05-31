3c2dc228324600a3c9407d7a872da21c1172a830
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-05-31 22:08:40 +0200

    Fix conf mat logging viz

--- eval/vis.py
+++ eval/vis.py
@@ -25,29 +25,31 @@ def confmat_to_fig(cm, class_names):
        cm (array, shape = [n, n]): a confusion matrix of integer classes
        class_names (array, shape = [n]): String names of the integer classes
     """
+    # move the class names to the top
     plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
     plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
 
+    # Normalize the confusion matrix.
+    # add 1 to prevent NaNs
+    cm_norm = cm.astype('float') / (cm.sum(axis=1)[:, np.newaxis] + 1)
+    # reduce precision
+    cm_norm = np.around(cm_norm, decimals=2)
+
     figure = plt.figure(figsize=(16, 16))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    # set min and max values
+    plt.imshow(cm_norm, interpolation='nearest', cmap=plt.cm.Blues, vmin=0, vmax=1)
     plt.title("Confusion matrix")
     plt.colorbar()
     tick_marks = np.arange(len(class_names))
     plt.xticks(tick_marks, class_names, rotation=45)
     plt.yticks(tick_marks, class_names)
     
-    # Normalize the confusion matrix.
-    # add 1 to prevent NaNs
-    cm = cm.astype('float') / (cm.sum(axis=1)[:, np.newaxis] + 1)
-    # reduce precision
-    cm = np.around(cm, decimals=2)
-    
     # Use white text if squares are dark; otherwise black.
-    threshold = cm.max() / 2.
+    threshold = 0.5
     
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        color = "white" if cm[i, j] > threshold else "black"
-        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+    for i, j in itertools.product(range(cm_norm.shape[0]), range(cm_norm.shape[1])):
+        color = "white" if cm_norm[i, j] > threshold else "black"
+        plt.text(j, i, cm_norm[i, j], horizontalalignment="center", color=color)
         
     plt.tight_layout()
     plt.ylabel('True label')
