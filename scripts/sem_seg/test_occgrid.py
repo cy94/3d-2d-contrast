fd8cd67ed012b1786bc7e03b8d77b7180e2b8518
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-02 12:27:10 +0200

    Complete test script

--- scripts/sem_seg/test_occgrid.py
+++ scripts/sem_seg/test_occgrid.py
@@ -3,15 +3,10 @@ import argparse
 
 
 from lib.misc import read_config
-from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid, ScanNetGridTestSubvols, collate_func
-from transforms.grid_3d import AddChannelDim, TransposeDims
+from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid
+
 from models.sem_seg.utils import count_parameters
 from models.sem_seg.fcn3d import FCN3D
-
-from torchinfo import summary
-import torch
-from torch.utils.data import DataLoader
-from torchvision.transforms import Compose
 
 import pytorch_lightning as pl
 
@@ -22,25 +17,10 @@ def main(args):
     model.eval()
     print(f'Num params: {count_parameters(model)}')
 
-    ckpt = torch.load(cfg['test']['ckpt'], map_location='cpu')
-    train_cfg = ckpt['hyper_parameters']['cfg']
-
-    # create transforms list
-    transforms = []
-    transforms.append(AddChannelDim())
-    transforms.append(TransposeDims())
-    t = Compose(transforms)
-
     test_set = ScanNetSemSegOccGrid(cfg['data'], split='test', full_scene=True)
     print(f'Test set: {len(test_set)}')
 
-    for scene in test_set:
-        subvols = ScanNetGridTestSubvols(scene, train_cfg['data']['subvol_size'], 
-                                        target_padding=train_cfg['data']['target_padding'], 
-                                        transform=t)
-        test_loader = DataLoader(subvols, batch_size=cfg['test']['batch_size'],
-                                shuffle=False, num_workers=8, collate_fn=collate_func,
-                                pin_memory=True) 
+    model.test_scenes(test_set, cfg)
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
