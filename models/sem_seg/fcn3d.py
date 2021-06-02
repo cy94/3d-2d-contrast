af4bb2916064245b9160142ef352658d953b0192
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-02 15:47:10 +0200

    Increase model capacity

--- models/sem_seg/fcn3d.py
+++ models/sem_seg/fcn3d.py
@@ -56,20 +56,20 @@ class FCN3D(pl.LightningModule):
             nn.ReLU(),
 
             # 1/2->1/4
-            nn.Conv3d(32, 32, 3, 2, 1),
-            # same
-            nn.Conv3d(32, 64, 3, 1, 1),
-            nn.ReLU(),
-            
-            # 1.4->1/8
-            nn.Conv3d(64, 64, 3, 2, 1),
+            nn.Conv3d(32, 64, 3, 2, 1),
             # same
             nn.Conv3d(64, 64, 3, 1, 1),
             nn.ReLU(),
             
+            # 1.4->1/8
+            nn.Conv3d(64, 128, 3, 2, 1),
+            # same
+            nn.Conv3d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            
             # inchannels, outchannels, kernel, stride, padding, output_padding
             # 1/8->1/4
-            nn.ConvTranspose3d(64, 64, 4, 2, 1),
+            nn.ConvTranspose3d(128, 64, 4, 2, 1),
             nn.ReLU(),
             # 1/4->1/2
             nn.ConvTranspose3d(64, 64, 4, 2, 1),
