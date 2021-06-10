2edbb74650eda72a0f3ca8831cab159b01f67e52
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-10 15:00:24 +0200

    Add SparseResUNet to train script

--- scripts/sem_seg/train_occgrid_lt.py
+++ scripts/sem_seg/train_occgrid_lt.py
@@ -6,6 +6,7 @@ from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid, collate_func
 from transforms.grid_3d import AddChannelDim, DenseToSparse, TransposeDims, MapClasses
 from models.sem_seg.utils import count_parameters
 from models.sem_seg.fcn3d import FCN3D, SparseNet3D, UNet3D
+from models.sem_seg.sparse.res16unet import SparseResUNet
 
 from torchinfo import summary
 from torch.utils.data import Subset, DataLoader
@@ -15,13 +16,17 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 
+SPARSE_MODELS = 'SparseNet3D', 'SparseResUNet'
+
 def main(args):
     cfg = read_config(args.cfg_path)
     model_name = cfg['model']['name']
+    is_sparse = model_name in SPARSE_MODELS
+
     # create transforms list
     # map none class to padding, no loss on this class
     transforms = [MapClasses({0: cfg['data']['target_padding']})]
-    if model_name in ('SparseNet3D',):
+    if model_name in SPARSE_MODELS:
         transforms.append(DenseToSparse())
     else:
         transforms.append(AddChannelDim())
@@ -29,10 +34,10 @@ def main(args):
     t = Compose(transforms)
 
     if cfg['data']['train_list'] and cfg['data']['val_list']:
-        train_set = ScanNetSemSegOccGrid(cfg['data'], transform=t, split='train')
-        val_set = ScanNetSemSegOccGrid(cfg['data'], transform=t, split='val')
+        train_set = ScanNetSemSegOccGrid(cfg['data'], transform=t, split='train', full_scene=is_sparse)
+        val_set = ScanNetSemSegOccGrid(cfg['data'], transform=t, split='val', full_scene=is_sparse)
     else:
-        dataset = ScanNetSemSegOccGrid(cfg['data'], transform=t)
+        dataset = ScanNetSemSegOccGrid(cfg['data'], transform=t, full_scene=is_sparse)
         print(f'Full dataset size: {len(dataset)}')
         if cfg['train']['train_split']:
             train_size = int(cfg['train']['train_split'] * len(dataset))
@@ -51,7 +56,7 @@ def main(args):
     print(f'Prepare a fixed val set')
     val_set = [s for s in val_set]
 
-    cfunc = SparseNet3D.collation_fn if cfg['model']['name'] == 'SparseNet3D' \
+    cfunc = SparseNet3D.collation_fn if model_name in SPARSE_MODELS \
         else collate_func
 
     train_loader = DataLoader(train_set, batch_size=cfg['train']['train_batch_size'],
@@ -66,8 +71,9 @@ def main(args):
         'FCN3D': FCN3D,
         'UNet3D': UNet3D,
         'SparseNet3D': SparseNet3D,
+        'SparseResUNet': SparseResUNet
     }
-    model = models[cfg['model']['name']](in_channels=1, num_classes=21, cfg=cfg)
+    model = models[model_name](in_channels=1, num_classes=21, cfg=cfg)
     print(f'Num params: {count_parameters(model)}')
 
     input_size = (cfg['train']['train_batch_size'], 1,) + tuple(cfg['data']['subvol_size'])
@@ -77,9 +83,6 @@ def main(args):
     except:
         pass
 
-    if cfg['train']['resume']:
-        pass
-    
     checkpoint_callback = ModelCheckpoint(save_last=True, save_top_k=5, 
                                         monitor='loss/val')
 
