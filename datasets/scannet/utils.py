403eaaa784ccbc4e11fd9bb5eff07539e9c00d46
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-14 19:19:19 +0200

    Update get_ dataset and loader functions, move common things to common.py

--- datasets/scannet/utils.py
+++ datasets/scannet/utils.py
@@ -1,161 +1,174 @@
-import os
-import csv
-import numpy as np
+from transforms.common import ComposeCustom
+from models.sem_seg.utils import SPARSE_MODELS
+from transforms.sparse_3d import ElasticDistortion, RandomDropout, RandomHorizontalFlip
+from datasets.scannet.sparse_3d import ScannetVoxelization2cmDataset, ScannetVoxelizationDataset
 
-CLASS_NAMES = ['none', 'wall', 'floor', 'cabinet', 
-            'bed', 'chair', 'sofa', 'table', 
-            'door', 'window', 'bookshelf', 'picture', 
-            'counter', 'desk', 'curtain', 'refrigerator', 
-            'shower_curtain', 'toilet', 'sink', 'bathtub', 
-            'otherfurn', 
-            'padding']
+import torch
+from torchvision.transforms import Compose
+from torch.utils.data import Subset, DataLoader
+import MinkowskiEngine as ME
+
+from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid, collate_func
+
+from transforms.grid_3d import DenseToSparse, RandomTranslate, RandomRotate, \
+    MapClasses, AddChannelDim, TransposeDims
+
+
+class cfl_collate_fn_factory:
+  """Generates collate function for coords, feats, labels.
+
+    Args:
+      limit_numpoints: If 0 or False, does not alter batch size. If positive integer, limits batch
+                       size so that the number of input coordinates is below limit_numpoints.
+  """
+
+  def __init__(self, limit_numpoints):
+    self.limit_numpoints = limit_numpoints
+
+  def __call__(self, list_data):
+    coords, feats, labels = list(zip(*list_data))
+    coords_batch, feats_batch, labels_batch = [], [], []
+
+    batch_id = 0
+    batch_num_points = 0
+    for batch_id, _ in enumerate(coords):
+      num_points = coords[batch_id].shape[0]
+      batch_num_points += num_points
+      if self.limit_numpoints and batch_num_points > self.limit_numpoints:
+        num_full_points = sum(len(c) for c in coords)
+        num_full_batch_size = len(coords)
+        print(
+            f'\t\tCannot fit {num_full_points} points into {self.limit_numpoints} points '
+            f'limit. Truncating batch size at {batch_id} out of {num_full_batch_size} with {batch_num_points - num_points}.'
+        )
+        break
+      coords_batch.append(torch.from_numpy(coords[batch_id]).int())
+      feats_batch.append(torch.from_numpy(feats[batch_id]))
+      labels_batch.append(torch.from_numpy(labels[batch_id]).int())
+
+      batch_id += 1
+
+    # Concatenate all lists
+    coords_batch, feats_batch, labels_batch = ME.utils.sparse_collate(coords_batch, feats_batch, labels_batch)
+
+    return {
+        'coords': coords_batch,
+        'feats': feats_batch.float(),
+        'y': labels_batch.long()
+    } 
+
+def get_collate_func(cfg):
+    if cfg['model']['name'] in SPARSE_MODELS:
+        return cfl_collate_fn_factory(0)
+    else:
+        return collate_func
+
+def get_transform(cfg, mode):
+    '''
+    cfg: the full train cfg
+    mode: train or val
+    '''
+    train = (mode == 'train')
+    model_name = cfg['model']['name']
+    # create transforms list
+    # map none class to padding, no loss on this class
+    transforms = [
+        MapClasses({0: cfg['data']['target_padding']}),
+        ]
+    if train: transforms.append(RandomRotate())
+    # augmentations for sparse conv
+    if model_name in SPARSE_MODELS:
+        transforms.append(DenseToSparse())
+        if train: transforms.append(RandomTranslate())
+    # augmentations for dense conv
+    else:
+        transforms.append(AddChannelDim())
+        transforms.append(TransposeDims())
+    t = Compose(transforms)
+
+    return t
+
+def get_trainval_loaders(train_set, val_set, cfg):
+    cfunc = get_collate_func(cfg)
     
-'''
-1/class fraction weights
+    train_loader = DataLoader(train_set, batch_size=cfg['train']['train_batch_size'],
+                            shuffle=True, num_workers=8, collate_fn=cfunc,
+                            pin_memory=True)  
 
-obtained by 
-counts = np.bincount(labels)
-fraction = counts / counts.sum()
-weights = 1/fraction
-'''
-CLASS_WEIGHTS = [
-    # 0=none - doesnt matter
-     1, 0.0014, 0.002, 0.01, 
-     0.015, 0.006, 0.015, 0.01, 
-     0.01, 0.014, 0.02, 0.085, 
-     0.08, 0.021, 0.024, 0.078, 
-     0.16, 0.14, 0.17, 0.12, 
-     0.01
- ]
+    val_loader = DataLoader(val_set, batch_size=cfg['train']['val_batch_size'],
+                            shuffle=False, num_workers=8, collate_fn=cfunc,
+                            pin_memory=True) 
+                            
+    return train_loader, val_loader
 
-VALID_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28, 33, 34, 36, 39] 
+def get_trainval_sparse(cfg):
+    prevoxel_transform = ComposeCustom([
+       ElasticDistortion(ScannetVoxelizationDataset.ELASTIC_DISTORT_PARAMS) 
+    ]) 
 
-def read_list(path):
-    '''
-    read list of lines from a file
-    path: Path object or str
-    '''
-    with open(path) as f:
-        lines = f.readlines()
+    input_transform = ComposeCustom([
+        RandomDropout(0.2),
+        RandomHorizontalFlip(ScannetVoxelizationDataset.ROTATION_AXIS, \
+                                ScannetVoxelizationDataset.IS_TEMPORAL),
+    ])
 
-    cleanlines = [line.strip() for line in lines]
+    train_set = ScannetVoxelization2cmDataset(
+                    cfg,
+                    prevoxel_transform=prevoxel_transform,
+                    input_transform=input_transform,
+                    target_transform=None,
+                    cache=False,
+                    augment_data=True,
+                    phase='train')
 
-    return cleanlines
+    val_set = ScannetVoxelization2cmDataset(
+                    cfg,
+                    prevoxel_transform=prevoxel_transform,
+                    input_transform=input_transform,
+                    target_transform=None,
+                    cache=False,
+                    augment_data=False,
+                    phase='val')
 
-def nyu40_to_continuous(img):
-    '''
-    map NYU40 labels 0-40 in VALID_CLASSES to continous labels 0-20
-    '''
-    new_img = img.copy()
-    valid_to_cts = dict(zip(VALID_CLASSES, range(len(VALID_CLASSES))))
+    return train_set, val_set
 
-    for nyu_cls in range(41):
-        if nyu_cls in VALID_CLASSES:
-            new_img[img == nyu_cls] = valid_to_cts[nyu_cls]
+def get_trainval_dense(cfg):
+    # basic transforms + augmentation
+    train_t = get_transform(cfg, 'train')
+    # basic transforms, no augmentation
+    val_t = get_transform(cfg, 'val')
+
+    if cfg['data']['train_list'] and cfg['data']['val_list']:
+        train_set = ScanNetSemSegOccGrid(cfg['data'], transform=train_t, split='train')
+        val_set = ScanNetSemSegOccGrid(cfg['data'], transform=val_t, split='val')
+    else:
+        dataset = ScanNetSemSegOccGrid(cfg['data'], transform=None)
+        print(f'Full dataset size: {len(dataset)}')
+        if cfg['train']['train_split']:
+            train_size = int(cfg['train']['train_split'] * len(dataset))
+            train_set = Subset(dataset, range(train_size))
+            val_set = Subset(dataset, range(train_size, len(dataset)))
+        elif cfg['train']['train_size'] and cfg['train']['val_size']:
+            train_set = Subset(dataset, range(cfg['train']['train_size']))
+            val_set = Subset(dataset, range(cfg['train']['train_size'], 
+                                cfg['train']['train_size']+cfg['train']['val_size']))
         else:
-            new_img[img == nyu_cls] = 0
-
-    return new_img
-
-def continous_to_nyu40(img):
-    '''
-    map continous labels 0-20 NYU40 labels 0-40 in VALID_CLASSES
-    img: single image (h, w) or batch (n, h, w)
-    '''
-    new_img = img.copy()
-
-    cts_to_valid = dict(zip(range(len(VALID_CLASSES)), VALID_CLASSES))
-
-    for cts_cls in range(21):
-        new_img[img == cts_cls] = cts_to_valid[cts_cls]
+            raise ValueError('Train val split not specified')
+        train_set.transform = train_t
+        val_set.transform = val_t
     
-    return new_img
+    return train_set, val_set
 
-def viz_labels(img):
+def get_trainval_sets(cfg):
     '''
-    Map NYU40 labels to corresponding RGB values
-    img: single image (h, w) or batch (n, h, w)
+    get train and val sets 
+    cfg: full train cfg
     '''
-    vis_image = np.zeros(img.shape +(3,), dtype=np.uint8)
+    is_sparse = cfg['model']['name'] in SPARSE_MODELS
 
-    color_palette = create_color_palette()
-
-    for idx, color in enumerate(color_palette):
-        vis_image[img == idx] = color
-
-    return vis_image
-
-# NYU labels
-def create_color_palette():
-    colors =  [
-       (0, 0, 0), # first index=0
-       (174, 199, 232),  # 1.wall
-       (152, 223, 138),  # 2.floor
-       (31, 119, 180),   # 3.cabinet
-       (255, 187, 120),  # 4.bed
-       (188, 189, 34),   # 5.chair
-       (140, 86, 75),    # 6.sofa
-       (255, 152, 150),  # 7.table
-       (214, 39, 40),    # 8.door
-       (197, 176, 213),  # 9.window
-       (148, 103, 189),  # 10.bookshelf
-       (196, 156, 148),  # 11.picture
-       (23, 190, 207),   # 12.counter
-       (178, 76, 76),  
-       (247, 182, 210),  # 14.desk
-       (66, 188, 102), 
-       (219, 219, 141),  # 16.curtain
-       (140, 57, 197), 
-       (202, 185, 52), 
-       (51, 176, 203), 
-       (200, 54, 131), 
-       (92, 193, 61),  
-       (78, 71, 183),  
-       (172, 114, 82), 
-       (255, 127, 14),   # 24.refrigerator
-       (91, 163, 138), 
-       (153, 98, 156), 
-       (140, 153, 101),
-       (158, 218, 229),  # 28.shower curtain
-       (100, 125, 154),
-       (178, 127, 135),
-       (120, 185, 128),
-       (146, 111, 194),
-       (44, 160, 44),    # 33.toilet
-       (112, 128, 144),  # 34.sink
-       (96, 207, 209), 
-       (227, 119, 194),  # 36.bathtub
-       (213, 92, 176), 
-       (94, 106, 211), 
-       (82, 84, 163),    # 39.otherfurn
-       (100, 85, 144)    # last index=40
-    ]
-    return colors
-
-# map scannet -> nyu40
-def map_labels(arr, label_mapping):
-    mapped = np.copy(arr)
-    for k,v in label_mapping.items():
-        mapped[arr == k] = v
-    return mapped.astype(np.uint8)
-
-# if string s represents an int
-def represents_int(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-# read the TSV file
-def read_label_mapping(filename, label_from='id', label_to='nyu40id'):
-    assert os.path.isfile(filename)
-    mapping = dict()
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='\t')
-        for row in reader:
-            mapping[row[label_from]] = int(row[label_to])
-    # if ints convert 
-    if represents_int(list(mapping.keys())[0]):
-        mapping = {int(k):v for k,v in mapping.items()}
-    return mapping
+    if is_sparse:
+        train_set, val_set = get_trainval_sparse(cfg)
+    else:
+        train_set, val_set = get_trainval_dense(cfg)
+        
+    return train_set, val_set
