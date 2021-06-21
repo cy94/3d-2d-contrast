d7ce8f39e88b7ce1698a0dd34a4aa3c07cd838fe
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-21 22:01:29 +0200

    Update sparse test script

--- scripts/sem_seg/test_occgrid.py
+++ scripts/sem_seg/test_occgrid.py
@@ -1,36 +1,30 @@
 
 import argparse
+from datasets.scannet.utils import get_trainval_loaders, get_trainval_sets
 
 import torch
 
 from lib.misc import read_config
-from datasets.scannet.sem_seg_3d import ScanNetSemSegOccGrid
 
-from models.sem_seg.utils import count_parameters, MODEL_MAP, get_transform
-
-import pytorch_lightning as pl
+from models.sem_seg.utils import MODEL_MAP
 
 def main(args):
     cfg = read_config(args.cfg_path)
     train_cfg = torch.load(cfg['test']['ckpt'])['hyper_parameters']['cfg']
 
-    # create transforms list
-    t = get_transform(train_cfg, 'val')
+    _, test_set = get_trainval_sets(train_cfg)
+    print(f'Val set: {len(test_set)}')
+    _, test_loader = get_trainval_loaders([], test_set, train_cfg)
 
     model = MODEL_MAP[train_cfg['model']['name']].load_from_checkpoint(cfg['test']['ckpt'],
-                                                in_channels=1, num_classes=21)
+                                                in_channels=3, num_classes=20)
     model.eval()
-    print(f'Num params: {count_parameters(model)}')
 
-    test_set = ScanNetSemSegOccGrid(cfg['data'], split='test', full_scene=True)
-    print(f'Test set: {len(test_set)}')
-
-    model.test_scenes(test_set, cfg, transform=t)
+    model.test_scenes(test_loader)
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('cfg_path', help='Path to cfg')
-    parser = pl.Trainer.add_argparse_args(p)
     args = p.parse_args()
 
     main(args)
