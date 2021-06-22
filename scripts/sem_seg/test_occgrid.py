cfd46145fad4409530256813c6a7b48795989982
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-06-22 16:16:45 +0200

    Use cpu/gpu in sparse test script

--- scripts/sem_seg/test_occgrid.py
+++ scripts/sem_seg/test_occgrid.py
@@ -9,14 +9,18 @@ from lib.misc import read_config
 from models.sem_seg.utils import MODEL_MAP
 
 def main(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device', device)
+
     cfg = read_config(args.cfg_path)
-    train_cfg = torch.load(cfg['test']['ckpt'])['hyper_parameters']['cfg']
+    train_cfg = torch.load(cfg['test']['ckpt'], map_location=device)['hyper_parameters']['cfg']
 
     _, test_set = get_trainval_sets(train_cfg)
     print(f'Val set: {len(test_set)}')
     _, test_loader = get_trainval_loaders([], test_set, train_cfg)
 
     model = MODEL_MAP[train_cfg['model']['name']].load_from_checkpoint(cfg['test']['ckpt'],
+                                                map_location=device,
                                                 in_channels=3, num_classes=20)
     model.eval()
 
