0e4755cfad7510631b076662c5e061dafd54f651
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-07-05 21:15:33 +0200

    Update enet train script to lightning

--- scripts/sem_seg/train_enet.py
+++ scripts/sem_seg/train_enet.py
@@ -1,25 +1,18 @@
 import os 
 import argparse
-from datetime import datetime as dt
-import random 
-
-import numpy as np
-
-from tqdm import tqdm
-
 from lib.misc import read_config
 from datasets.scannet.sem_seg_2d import ScanNetSemSeg2D, collate_func
 from transforms.image_2d import Normalize, TransposeChannels, Resize
 from models.sem_seg.enet import ENet2
 from models.sem_seg.utils import count_parameters
-from eval.sem_seg_2d import miou
 
-import torch
-from torch.optim import Adam
-import torch.nn.functional as F
 from torch.utils.data import Subset, DataLoader
+from torchinfo import summary
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
 from torchvision.transforms import Compose
-from torch.utils.tensorboard import SummaryWriter
 
 
 def main(args):
@@ -34,20 +27,13 @@ def main(args):
 
     t = Compose(transforms)
 
-    dataset = ScanNetSemSeg2D(cfg['data']['root'], cfg['data']['label_file'],
-                                cfg['data']['limit_scans'],
-                                transform=t, frame_skip=cfg['data']['frame_skip'])
+    train_set = ScanNetSemSeg2D(cfg, transform=t, split='train')
+    val_set = ScanNetSemSeg2D(cfg, transform=t, split='val')        
 
-    if cfg['train']['train_split']:
-        train_size = int(cfg['train']['train_split'] * len(dataset))
-        train_set = Subset(dataset, range(train_size))
-        val_set = Subset(dataset, range(train_size, len(dataset)))
-    elif cfg['train']['train_size'] and cfg['train']['val_size']:
-        train_set = Subset(dataset, range(cfg['train']['train_size']))
-        val_set = Subset(dataset, range(cfg['train']['train_size'], 
-                            cfg['train']['train_size']+cfg['train']['val_size']))
-    else:
-        raise ValueError('Train val split not specified')
+    if args.subset:
+        print('Select a subset of data for quick run')
+        train_set = Subset(train_set, range(128))
+        val_set = Subset(val_set, range(128))                        
 
     print(f'Train set: {len(train_set)}')
     print(f'Val set: {len(val_set)}')
@@ -57,100 +43,46 @@ def main(args):
 
     val_loader = DataLoader(val_set, batch_size=cfg['train']['val_batch_size'],
                             shuffle=False, num_workers=4, collate_fn=collate_func)      
-    val_loader_shuffle = DataLoader(val_set, batch_size=cfg['train']['val_batch_size'],
-                        shuffle=True, num_workers=4, collate_fn=collate_func)                                  
 
+    # ignored label in 2D is called target padding in 3D - use a common name 
+    cfg['data']['target_padding'] = cfg['data']['ignore_label']
+    model = ENet2(num_classes=20, cfg=cfg)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = ENet2(num_classes=21).to(device)
-    print(f'Num params: {count_parameters(model)}')
-    optimizer = Adam(model.parameters(), lr=cfg['train']['lr'], weight_decay=cfg['train']['l2'])
-    
-    save_dir = 'runs/' + dt.now().strftime('%d-%b-%H.%M.%S')
-    ckpt_dir = f'{save_dir}/ckpt'
-    
-    if args.quick_run:
-        print('Quick run')
-        cfg['train']['epochs'] = 1
-    else:
-        os.makedirs(ckpt_dir, exist_ok=True)
-        writer = SummaryWriter(log_dir=f'{save_dir}')
-    
-    print(f'Save dir: {save_dir}')
-    
-    step = 0
-    start_epoch = 0
-    
-    if cfg['train']['resume']:
-        print(f'Resuming from ckpt: ', cfg['train']['resume'])
-        ckpt = torch.load(cfg['train']['resume'])
-        model.load_state_dict(ckpt['model_state_dict'])
-        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
-        start_epoch = ckpt['epoch']
-        step = (start_epoch + 1) * (len(train_set) // ckpt['cfg']['train']['train_batch_size'])
+    input_size = (cfg['train']['train_batch_size'], 3,) + tuple(cfg['data']['img_size'][::-1])
+    summary(model, input_size=input_size)
 
-    for epoch in tqdm(range(start_epoch, cfg['train']['epochs']), desc='epoch'):
-        for batch in tqdm(train_loader, desc='train', leave=False):
-            model.train()
-            step += 1
-            img, label = batch['img'].to(device), batch['label'].to(device)
-            optimizer.zero_grad()
-            out = model(img)
-            loss = F.cross_entropy(out, label)
-            loss.backward()
-            optimizer.step()
+    callbacks = [LearningRateMonitor(logging_interval='step')]
+    if not args.no_ckpt:
+        print('Saving checkpoints')
+        callbacks.append(ModelCheckpoint(save_last=True, save_top_k=5, 
+                                        monitor='iou/val/mean',
+                                        mode='max',
+                                # put the miou in the filename
+                                filename='epoch{epoch:02d}-step{step}-miou{iou/val/mean:.2f}',
+                                auto_insert_metric_name=False))
+    ckpt = cfg['train'].get('resume', None)
+    if ckpt is not None:
+        print(f'Resuming from checkpoint: {ckpt}')
 
-            # log train loss
-            if not args.quick_run:
-                writer.add_scalar('loss/train', loss / len(batch), step)
+    trainer = pl.Trainer(resume_from_checkpoint=ckpt,
+                        gpus=1, 
+                        log_every_n_steps=10,
+                        callbacks=callbacks,
+                        max_epochs=cfg['train']['epochs'],
+                        val_check_interval=cfg['train']['eval_intv'],
+                        fast_dev_run=args.fast_dev_run,
+                        accumulate_grad_batches=cfg['train'].get('accum_grad', 1))
+    trainer.fit(model, train_loader, val_loader)
 
-            # compute train miou for a few random batches
-            if random.random() > 0.8:  
-                preds = out.argmax(dim=1)
-                miou_train = miou(preds, label, 21)
-                if not np.isnan(miou_train).any() and not args.quick_run:
-                    writer.add_scalar('miou/train', miou_train.mean(), step)            
-
-            # evaluate at intervals?
-            if step % cfg['train']['eval_intv'] == 0:
-                model.eval()
-                val_loss = 0
-                n_batches = 0
-
-                with torch.no_grad():
-                    for batch_ndx, batch in enumerate(tqdm(val_loader, desc='val', leave=False)):
-                        n_batches += 1
-                        img, label = batch['img'].to(device), batch['label'].to(device)
-                        out = model(img)
-                        val_loss += (F.cross_entropy(out, label) / len(batch))
-
-                if not args.quick_run:
-                    writer.add_scalar('loss/val', val_loss / n_batches, step)     
-                
-                # pick a random batch to compute val miou
-                batch = next(iter(val_loader_shuffle))
-                img, label = batch['img'].to(device), batch['label'].to(device)
-                preds = model(img).argmax(dim=1)
-                miou_val = miou(preds, label, 21)
-
-                if not np.isnan(miou_val).any() and not args.quick_run:
-                    writer.add_scalar('miou/val', miou_val.mean(), step)   
-
-        if (not args.no_ckpt) \
-            and (not args.quick_run) \
-            and (epoch % cfg['train']['ckpt_intv'] == 0):
-            torch.save({
-                'cfg': cfg,
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                }, f'{ckpt_dir}/{epoch}.pt')    
   
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('cfg_path', help='Path to cfg')
-    p.add_argument('--quick', dest='quick_run', action='store_true', help='Quick run?')
-    p.add_argument('--no-ckpt', dest='no_ckpt', action='store_true', help='Dont store checkpoints')
+    p.add_argument('--no-ckpt', action='store_true', dest='no_ckpt', 
+                    default=False, help='Dont store checkpoints (for debugging)')
+    p.add_argument('--subset', action='store_true', dest='subset', 
+                    default=False, help='Use a subset of dataset')
+    parser = pl.Trainer.add_argparse_args(p)
     args = p.parse_args()
 
     main(args)
