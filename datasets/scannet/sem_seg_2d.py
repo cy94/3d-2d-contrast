9a57f63c211aa31a12ea7d53aec20da752a51ce2
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-07-05 21:13:46 +0200

    Use train/val lists in 2d dataset, map all ignored classes to ignore label

--- datasets/scannet/sem_seg_2d.py
+++ datasets/scannet/sem_seg_2d.py
@@ -2,6 +2,7 @@
 2D semantic segmentation on ScanNet images
 '''
 
+from datasets.scannet.common import read_list
 from pathlib import Path
 import os, os.path as osp
 
@@ -11,15 +12,15 @@ import imageio
 import torch
 from torch.utils.data import Dataset
 
-from datasets.scannet.utils import read_label_mapping, map_labels, nyu40_to_continuous
+from datasets.scannet.common import read_label_mapping, map_labels, nyu40_to_continuous
 
 
 def collate_func(sample_list):
     return {
         'img_path': [s['img_path'] for s in sample_list],
         'label_path': [s['label_path'] for s in sample_list],
-        'img': torch.Tensor([s['img'] for s in sample_list]),
-        'label': torch.LongTensor([s['label'] for s in sample_list]),
+        'x': torch.Tensor([s['img'] for s in sample_list]),
+        'y': torch.LongTensor([s['label'] for s in sample_list]),
     }
 
 class ScanNetSemSeg2D(Dataset):
@@ -27,31 +28,33 @@ class ScanNetSemSeg2D(Dataset):
     ScanNet 2d semantic segmentation dataset
 
     images: RGB 
-    labels: 0-20 
+    labels: 0-19 (20 classes) + 20 (ignored classes) 
 
     labels as reported here: http://kaldir.vc.in.tum.de/scannet_benchmark/
     '''
-    def __init__(self, root_dir, label_file, limit_scans=None, transform=None,
-                frame_skip=None):
+    def __init__(self, cfg, split, transform=None):
         '''
-        root_dir: contains scan dirs scene0000_00, etc
-        label_file: path to scannetv2-labels.combined.tsv
-        limit_scans: read these many scans in sorted order
+        cfg: the full train cfg
+        split: train/val/None (all)
         transform: apply on each sample
-        frame_skip: skip every n frames within every scan
         '''
-        self.root_dir = Path(root_dir)
+        self.root_dir = Path(cfg['data']['root'])
+        # store data paths
         self.img_paths = []
         self.label_paths = []
+
         self.transform = transform
+        self.frame_skip = cfg['data'].get('frame_skip', 1)
+        self.scannet_to_nyu40 = read_label_mapping(cfg['data']['label_file'])
 
-        self.frame_skip = frame_skip if frame_skip is not None else 1
+        # use a fixed set of scans
+        if split:
+            scans = read_list(cfg['data'][f'{split}_list'])
+        else:
+        # use all scans in this dir
+            scans = sorted(os.listdir(self.root_dir))
 
-        self.scannet_to_nyu40 = read_label_mapping(label_file)
-
-        scans = sorted(os.listdir(self.root_dir))
-        if limit_scans:
-            scans = scans[:limit_scans]
+        self.ignore_label = cfg['data']['ignore_label']
 
         for scan_id in scans:
             scan_dir = self.root_dir / scan_id
@@ -62,31 +65,32 @@ class ScanNetSemSeg2D(Dataset):
             color_files = sorted(os.listdir(color_dir), key=lambda f: int(osp.splitext(f)[0]))
             # skip frames?
             for file_ndx in range(0, len(color_files), self.frame_skip):
+                # N.jpg
                 img_fname = color_files[file_ndx]
+                # just N
                 ndx = Path(img_fname).stem
-
+                # full image path
                 img_path = color_dir / img_fname
+                # full label path
                 label_path = label_dir / f'{ndx}.png'
-
+                # use this sample only if label exists
                 if label_path.exists():
                     self.img_paths.append(img_path)
                     self.label_paths.append(label_path)
-
-       
 
     def __len__(self):
         return len(self.img_paths)
     
     def __getitem__(self, idx):
         img_path, label_path = self.img_paths[idx], self.label_paths[idx]
-        
+        # read the image as float
         img = np.array(imageio.imread(img_path)).astype(np.float32)
-        
+        # read the scannet label image as int
         label_scannet = np.array(imageio.imread(label_path))
-        # map from scannet 0-40 to nyu 0-40
+        # map from scannet to nyu40 labels
         label_nyu40 = map_labels(label_scannet, self.scannet_to_nyu40)
-        # map from 0-40 to 0-20 continuous labels
-        label_selected = nyu40_to_continuous(label_nyu40)
+        # map from NYU40 to 0-19 + 20 (ignored) labels
+        label_selected = nyu40_to_continuous(label_nyu40, ignore_label=self.ignore_label)
 
         sample = {
             'img_path': img_path,
