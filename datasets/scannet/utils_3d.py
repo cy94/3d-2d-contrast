dbd9d28ae0c0031b2387ae84054c6d0f67a4bad2
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-07-21 12:28:56 +0200

    Read pose, depth, intrinsic and get coverage

---
+++ datasets/scannet/utils_3d.py
@@ -0,0 +1,284 @@
+import math
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import imageio
+
+def resize_crop_image(image, new_image_dims):
+    image_dims = [image.shape[1], image.shape[0]]
+    if image_dims == new_image_dims:
+        return image
+    resize_width = int(math.floor(new_image_dims[1] * float(image_dims[0]) / float(image_dims[1])))
+    image = transforms.Resize([new_image_dims[1], resize_width], 
+                    interpolation=transforms.InterpolationMode.NEAREST)(Image.fromarray(image))
+    image = transforms.CenterCrop([new_image_dims[1], new_image_dims[0]])(image)
+    image = np.array(image)
+
+    return image
+
+def load_depth(path, image_dims=(640, 480)):
+    '''
+    path: full path to depth file
+    image_dims: resize image to this size
+    '''
+    depth_image = imageio.imread(path)
+    depth_image = resize_crop_image(depth_image, image_dims)
+    depth_image = depth_image.astype(np.float32) / 1000.0
+    return depth_image
+    
+def load_pose(path):
+    '''
+    path: full path to a pose file
+    '''
+    return torch.from_numpy(np.genfromtxt(path).astype(np.float32))
+
+def load_intrinsic(path):
+    '''
+    path: full path to intrinsic file
+    '''
+    return torch.from_numpy(np.genfromtxt(path).astype(np.float32))
+
+# create camera intrinsics
+def make_intrinsic(fx, fy, mx, my):
+    '''
+    create intrinsic matrix from focal length and camera centers
+    '''
+    intrinsic = torch.eye(4)
+    intrinsic[0][0] = fx
+    intrinsic[1][1] = fy
+    intrinsic[0][2] = mx
+    intrinsic[1][2] = my
+    return intrinsic
+
+# create camera intrinsics
+def adjust_intrinsic(intrinsic, intrinsic_image_dim, image_dim):
+    '''
+    intrinsic: existing intrinsic matrix, corresponds to 640x480 image
+    intrinsic_image_dim: default 640x480, dims of image in the existing instrinsic matrix
+    image_dim: dims of the feature map, ~40x30
+    '''
+    # no need to change anything
+    if intrinsic_image_dim == image_dim:
+        return intrinsic
+    # keep the "30" dim fixed, find the corresponding width ~40
+    # ~ 30 * 640/480 = 40    
+    resize_width = int(math.floor(image_dim[1] * float(intrinsic_image_dim[0]) / float(intrinsic_image_dim[1])))
+    # multiply focal length x by a factor of (40/640) ~ 0.0625
+    intrinsic[0,0] *= float(resize_width)/float(intrinsic_image_dim[0])
+    # multiply focal length y by a factor of (30/480) ~ 0.0625 
+    intrinsic[1,1] *= float(image_dim[1])/float(intrinsic_image_dim[1])
+    # multiply the center of the image by the same factor 
+    # account for cropping here -> subtract 1
+    intrinsic[0,2] *= float(image_dim[0]-1)/float(intrinsic_image_dim[0]-1)
+    intrinsic[1,2] *= float(image_dim[1]-1)/float(intrinsic_image_dim[1]-1)
+    return intrinsic
+
+class ProjectionHelper():
+    def __init__(self, intrinsic, depth_min, depth_max, image_dims, volume_dims, voxel_size):
+        self.intrinsic = intrinsic
+        self.depth_min = depth_min
+        self.depth_max = depth_max
+        self.image_dims = image_dims
+        self.volume_dims = volume_dims
+        self.voxel_size = voxel_size
+
+    def update_intrinsic(self, new_intrinsic):
+        self.intrinsic = new_intrinsic
+
+    def depth_to_skeleton(self, ux, uy, depth):
+        '''
+        ux, uy: image coordinates
+        depth: depth to which these image coordinates must be projected 
+        '''
+        x = (ux - self.intrinsic[0][2]) / self.intrinsic[0][0]
+        y = (uy - self.intrinsic[1][2]) / self.intrinsic[1][1]
+        return torch.Tensor([depth*x, depth*y, depth])
+
+
+    def skeleton_to_depth(self, p):
+        '''
+        p: point in 3D
+        '''
+        x = (p[0] * self.intrinsic[0][0]) / p[2] + self.intrinsic[0][2]
+        y = (p[1] * self.intrinsic[1][1]) / p[2] + self.intrinsic[1][2]
+        return torch.Tensor([x, y, p[2]])
+
+    def compute_frustum_bounds(self, world_to_grid, camera_to_world):
+        '''
+        Given the location of the camera and the location of the grid
+        find the bounds of the grid that the camera can see
+        '''
+        # create an empty array with the same device and datatype as cam2world
+        # with dims: 
+            # 8: 8 points
+            # 4: homogenous coordinates (1 at the end)
+            # 1: value
+        corner_points = camera_to_world.new(8, 4, 1).fill_(1)
+	    # nearest frustum corners (depth min)
+        # lower left intrinsic will be used only 
+        corner_points[0][:3] = self.depth_to_skeleton(0, 0, self.depth_min).unsqueeze(1)
+        # lower right intrinsic will be used only 
+        corner_points[1][:3] = self.depth_to_skeleton(self.image_dims[0] - 1, 0, self.depth_min).unsqueeze(1)
+        # upper right intrinsic will be used only 
+        corner_points[2][:3] = self.depth_to_skeleton(self.image_dims[0] - 1, self.image_dims[1] - 1, self.depth_min).unsqueeze(1)
+        # upper left intrinsic will be used only 
+        corner_points[3][:3] = self.depth_to_skeleton(0, self.image_dims[1] - 1, self.depth_min).unsqueeze(1)
+        
+        # far frustum corners (depth max)
+        # lower left corner
+        corner_points[4][:3] = self.depth_to_skeleton(0, 0, self.depth_max).unsqueeze(1)
+        # lower right corner
+        corner_points[5][:3] = self.depth_to_skeleton(self.image_dims[0] - 1, 0, self.depth_max).unsqueeze(1)
+        # upper right corner
+        corner_points[6][:3] = self.depth_to_skeleton(self.image_dims[0] - 1, self.image_dims[1] - 1, self.depth_max).unsqueeze(1)
+        # upper left corner
+        corner_points[7][:3] = self.depth_to_skeleton(0, self.image_dims[1] - 1, self.depth_max).unsqueeze(1)
+
+        # go from camera coords to world coords - use cam2world matrix
+        p = torch.bmm(camera_to_world.repeat(8, 1, 1), corner_points)
+        # get a *range* of grid coords for these corner points
+        # p_lower: take floor of world coords, then map to grid coords
+        pl = torch.round(torch.bmm(world_to_grid.repeat(8, 1, 1), torch.floor(p)))
+        # p_upper: take ceil of world coords, then map to grid coords
+        pu = torch.round(torch.bmm(world_to_grid.repeat(8, 1, 1), torch.ceil(p)))
+
+        # remove the last 1 from homogenous coordinates
+        # in each case (rounded up/down) find the *coordinates* closest to the origin
+        bbox_min0, _ = torch.min(pl[:, :3, 0], 0)
+        bbox_min1, _ = torch.min(pu[:, :3, 0], 0)
+        # then take the minimum of those 2 cases
+        # -> get a single (x, y, z) 
+        bbox_min = np.minimum(bbox_min0, bbox_min1)
+        # repeat for maximum
+        bbox_max0, _ = torch.max(pl[:, :3, 0], 0)
+        bbox_max1, _ = torch.max(pu[:, :3, 0], 0) 
+        bbox_max = np.maximum(bbox_max0, bbox_max1)
+        return bbox_min, bbox_max
+
+    def get_coverage(self, depth, camera_to_world, world_to_grid):
+        pass
+
+    def compute_projection(self, depth, camera_to_world, world_to_grid):
+        '''
+        depth: a single depth image
+        cam2world: single transformation matrix
+        world2grid: single transformation matrix
+    
+        TODO: make runnable on CPU as well
+        '''
+        # compute projection by voxels -> image
+        # camera pose is camera->world, invert it
+        world_to_camera = torch.inverse(camera_to_world)
+        grid_to_world = torch.inverse(world_to_grid)
+        
+        # lowest xyz and highest xyz seen by the camera in grid coordsmask_frustum_bounds
+        # ie a bounding box of the frustum created by the camera 
+        # between depth_min and depth_max
+        voxel_bounds_min, voxel_bounds_max = self.compute_frustum_bounds(world_to_grid, 
+                                                                camera_to_world)
+        # min coords should be within the grid
+        voxel_bounds_min = np.maximum(voxel_bounds_min, 0).cuda()
+        # max coord should be within grid dimensions
+        voxel_bounds_max = np.minimum(voxel_bounds_max, self.volume_dims).float().cuda()
+
+        # coordinates within frustum bounds
+        # indices from 0,1,2 .. 31*31*62 = num_voxels
+        lin_ind_volume = torch.arange(0, self.volume_dims[0]*self.volume_dims[1]*self.volume_dims[2], out=torch.LongTensor()).cuda()
+        # homogenous coordinate XYZ of each voxel 
+        # (4, num_voxels)
+        coords = camera_to_world.new(4, lin_ind_volume.size(0))
+        # Z = N / (X*Y)
+        coords[2] = lin_ind_volume / (self.volume_dims[0]*self.volume_dims[1])
+        # similarly fill X and Y
+        tmp = lin_ind_volume - (coords[2]*self.volume_dims[0]*self.volume_dims[1]).long()
+        coords[1] = tmp / self.volume_dims[0]
+        coords[0] = torch.remainder(tmp, self.volume_dims[0])
+        # last coord is just 1
+        coords[3].fill_(1)
+       
+        # the actual voxels that the camera can see
+        # based on the lower bound
+        mask_frustum_bounds = torch.ge(coords[0], voxel_bounds_min[0]) \
+                            * torch.ge(coords[1], voxel_bounds_min[1]) \
+                            * torch.ge(coords[2], voxel_bounds_min[2])
+        # based on the upper bound
+        mask_frustum_bounds = mask_frustum_bounds \
+                            * torch.lt(coords[0], voxel_bounds_max[0]) \
+                            * torch.lt(coords[1], voxel_bounds_max[1]) \
+                            * torch.lt(coords[2], voxel_bounds_max[2])
+        # camera doesn't see any voxels                            
+        if not mask_frustum_bounds.any():
+            return None
+        
+        # pick only these voxels
+        lin_ind_volume = lin_ind_volume[mask_frustum_bounds]
+        # create new coordinates within the visible voxels (same as before)
+        # why?
+        coords = coords.resize_(4, lin_ind_volume.size(0))
+        coords[2] = lin_ind_volume / (self.volume_dims[0]*self.volume_dims[1])
+        tmp = lin_ind_volume - (coords[2]*self.volume_dims[0]*self.volume_dims[1]).long()
+        coords[1] = tmp / self.volume_dims[0]
+        coords[0] = torch.remainder(tmp, self.volume_dims[0])
+        coords[3].fill_(1)
+
+        # grid coords -> world coords -> camera coords XYZ
+        p = torch.mm(world_to_camera, torch.mm(grid_to_world, coords))
+
+        # project into image
+        p[0] = (p[0] * self.intrinsic[0][0]) / p[2] + self.intrinsic[0][2]
+        p[1] = (p[1] * self.intrinsic[1][1]) / p[2] + self.intrinsic[1][2]
+        pi = torch.round(p).long()
+
+        # check which image coords are valid
+        valid_ind_mask = torch.ge(pi[0], 0) \
+                        * torch.ge(pi[1], 0) \
+                        * torch.lt(pi[0], self.image_dims[0]) \
+                        * torch.lt(pi[1], self.image_dims[1])
+        if not valid_ind_mask.any():
+            return None
+
+        # valid X coords of image
+        valid_image_ind_x = pi[0][valid_ind_mask]
+        # valid Y coords of image
+        valid_image_ind_y = pi[1][valid_ind_mask]
+        # linear index into the image = Y + width*X
+        valid_image_ind_lin = valid_image_ind_y * self.image_dims[0] + valid_image_ind_x
+
+        # flatten the depth image, select the depth values corresponding 
+        # to the valid pixels
+        depth_vals = torch.index_select(depth.view(-1), 0, valid_image_ind_lin)
+        # depth > min_depth and depth < max_depth
+        # and depth is within voxel_size of 3D coordinates
+        depth_mask = depth_vals.ge(self.depth_min) \
+                    * depth_vals.le(self.depth_max) \
+                    * torch.abs(depth_vals - p[2][valid_ind_mask]).le(self.voxel_size)
+        # no valid depths
+        if not depth_mask.any():
+            return None
+
+        # pick the 3D indices which have valid 2D and valid depth
+        lin_ind_update = lin_ind_volume[valid_ind_mask]
+        lin_ind_update = lin_ind_update[depth_mask]
+        # create new tensors to store the indices
+        # each volume in the batch has a different number of valid voxels/pixels 
+        # but tensor shape needs to be same size for all in batch
+        # hence create tensor with the max size
+        # store the actual number of indices in the first element
+        # rest of the elements are the actual indices!
+
+        lin_indices_3d = lin_ind_update.new(self.volume_dims[0]*self.volume_dims[1]*self.volume_dims[2] + 1) 
+        lin_indices_2d = lin_ind_update.new(self.volume_dims[0]*self.volume_dims[1]*self.volume_dims[2] + 1) 
+
+        # 3d indices: indices of the valid voxels computed earlier
+        lin_indices_3d[0] = lin_ind_update.shape[0]
+        lin_indices_3d[1:1+lin_indices_3d[0]] = lin_ind_update
+        
+        # 2d indices: have the same shape
+        lin_indices_2d[0] = lin_ind_update.shape[0]
+        # values: the corresponding linear indices into the flattened image
+        # where depth values are nonzero
+        lin_indices_2d[1:1+lin_indices_2d[0]] = \
+            torch.index_select(valid_image_ind_lin, 0, torch.nonzero(depth_mask)[:,0])
+
+        return lin_indices_3d, lin_indices_2d
--- scripts/sem_seg/prep_backproj_data.py
+++ scripts/sem_seg/prep_backproj_data.py
@@ -3,13 +3,21 @@ prepare 2d+3d dataset (like 3DMV)
 3d: 32^3 subvolumes x and y sampled from dense grid
 2d: indices of 5 nearest images to each subvolume
 '''
-from datasets.scannet.sem_seg_3d import ScanNetPLYDataset
-from tqdm import tqdm
+import os, os.path as osp
 import argparse
 from pathlib import Path
+
+from torchvision.transforms import Compose
+from transforms.grid_3d import AddChannelDim, TransposeDims
+
+from tqdm import tqdm
+import imageio
 import h5py
-from lib.misc import read_config
 import numpy as np
+
+from lib.misc import read_config
+from datasets.scannet.sem_seg_3d import ScanNetPLYDataset
+from datasets.scannet.utils_3d import ProjectionHelper, adjust_intrinsic, load_depth, load_intrinsic, load_pose, make_intrinsic
 
 def create_datasets(out_file, n_samples, subvol_size, num_nearest_images):
     '''
@@ -18,7 +26,7 @@ def create_datasets(out_file, n_samples, subvol_size, num_nearest_images):
     out_file: h5py.File object opened in 'w' mode
     '''
     # input subvolume
-    out_file.create_dataset('x', (n_samples,) + subvol_size, dtype=np.float32)
+    out_file.create_dataset('x', (n_samples, 1) + subvol_size, dtype=np.float32)
     # label subvolume
     out_file.create_dataset('y', (n_samples,) + subvol_size, dtype=np.int16)
     # id of the scene that the volume came from (0000, 0002 ..)
@@ -29,6 +37,90 @@ def create_datasets(out_file, n_samples, subvol_size, num_nearest_images):
     out_file.create_dataset('world_to_grid', (n_samples, 4, 4), dtype=np.float32)
     # indices of the corresponding frames
     out_file.create_dataset('frames', (n_samples, 1), dtype=np.uint16)
+
+def add_translation(transform, t):
+    '''
+    transform: existing transformation matrix
+    t: additional translation
+    
+    add t to the existing translation
+    '''
+    new_transform = transform.copy()
+    new_transform[:3, 3] += t
+    return new_transform
+
+def get_world_to_scene(voxel_size, translation):
+    '''
+    standard world to scene transformation matrix
+    voxel_size: size of the grid voxels
+    translation: translation of the scene from the origin
+    '''
+    t = np.eye(4, dtype=np.float32) / voxel_size
+    # insert the translation
+    t[:3, 3] = translation
+    t[3, 3] = 1
+
+    return t
+
+def get_scene_scan_ids(path):
+    '''
+    path: /mnt/data/scannet/scans/scene0673_05/scene0673_05_voxelized.ply
+    output: 673, 05 (ints)
+    '''
+    return int(path.stem[5:9]), int(path.stem[10:12])
+
+def get_scan_name(path):
+    '''
+    path: /mnt/data/scannet/scans/scene0673_05/scene0673_05_voxelized.ply
+    output: scene0673_05
+    '''
+    return path.stem[:12]
+
+def get_nearest_images(world_to_grid, num_nearest_imgs, scan_name, root_dir,
+                        frame_skip, image_dims, projector):
+    '''
+    world_to_grid: location of the grid
+    num_nearest_imgs: only 1 supported now
+    scan_name: scene0000_00
+    root_dir: dir containing all scans with RGB and depth
+    frame_skip: number of frames to skip
+    image_dims: dims of the image used for projection 
+    projector: ProjectionHelper object
+    '''
+    root = Path(root_dir)
+    scan_dir = root / scan_name
+    pose_dir = scan_dir / 'pose'
+    depth_dir = scan_dir / 'depth'
+    intrinsic_path = root / scan_name / 'intrinsic/intrinsic_color.txt'
+    # set the intrinsic
+    intrinsic = load_intrinsic(intrinsic_path)
+    intrinsic = adjust_intrinsic(intrinsic, [1296, 968], image_dims)
+    projector.update_intrinsic(intrinsic)
+
+    # list all the camera poses
+    pose_files = sorted(os.listdir(pose_dir), key=lambda f: int(osp.splitext(f)[0]))
+    # actual poses considered
+    pose_indices = range(0, len(pose_files), frame_skip)
+    # 1 coverage int for each pose
+    coverage = np.zeros(len(pose_indices), dtype=np.uint16)
+
+    # iterate over camera pose files
+    for file_ndx in tqdm(pose_indices, leave=False, desc='pose'):
+        # N.txt
+        pose_fname = pose_files[file_ndx]
+        pose_path = pose_dir / pose_fname
+        # just N
+        ndx = Path(pose_fname).stem
+        depth_path = depth_dir / f'{ndx}.png'
+        # read pose and depth
+        depth = load_depth(depth_path)
+        pose = load_pose(pose_path)
+
+        # get coverage(depth)
+        # store the coverage of each image
+    # pick the image with max coverage
+    # return its index
+    return 1
 
 def main(args):
     cfg = read_config(args.cfg_path)
@@ -44,59 +136,65 @@ def main(args):
     subvols_per_scene = cfg['data']['subvols_per_scene']
     n_samples = len(dataset) * subvols_per_scene
     subvol_size = tuple(cfg['data']['subvol_size'])
-    num_nearest_images = cfg['data']['num_nearest_images']
+    num_nearest_imgs = cfg['data']['num_nearest_images']
+    img_size = cfg['data']['img_size']
 
     outfile = h5py.File(out_path, 'w')
-    create_datasets(outfile, n_samples, subvol_size, num_nearest_images)
+    create_datasets(outfile, n_samples, subvol_size, num_nearest_imgs)
 
     # iterate over each scene, read it only once
     data_ndx = 0
 
-    for _, scene in enumerate(tqdm(dataset)):
+    # intrinsic of the color camera from scene0001_00
+    intrinsic = make_intrinsic(1170.187988, 1170.187988, 647.75, 483.75)
+    # adjust for smaller image size
+    intrinsic = adjust_intrinsic(intrinsic, [1296, 968], cfg['data']['img_size'])
+    projector = ProjectionHelper(intrinsic, 
+                                cfg['data']['depth_min'], cfg['data']['depth_max'], 
+                                img_size,
+                                subvol_size,
+                                cfg['data']['voxel_size'])
+
+    for _, scene in enumerate(tqdm(dataset, desc='scene')):
         scene_x, scene_y, path = scene['x'], scene['y'], scene['path']
         scene_T = scene['translation']
+        # eg: 0000, 01 (int)
         scene_id, scan_id = get_scene_scan_ids(path)
+        # eg: scene0000_00 (str)
+        scan_name = get_scan_name(path)
         world_to_scene = get_world_to_scene(cfg['data']['voxel_size'], scene_T)
 
         # sample N subvols from this scene
-        for _ in tqdm(range(subvols_per_scene), leave=False):
+        for _ in tqdm(range(subvols_per_scene), leave=False, desc='subvol'):
             subvol_x, subvol_y, subvol_t = dataset.sample_subvol(scene_x, scene_y,
                                                     return_start_ndx=True)
+            # similarly reverse the order of axes: XYZ->ZYX
+            subvol_t = subvol_t                                                               
             # add the location of the grid                                                    
             world_to_grid = add_translation(world_to_scene, subvol_t)
 
-            outfile['x'][data_ndx] = subvol_x                                                    
-            outfile['y'][data_ndx] = subvol_y
+            # transform the volumes to the format used while training
+            x_final = AddChannelDim.apply(subvol_x)
+            x_final, y_final = TransposeDims.apply(x_final, subvol_y)
+
+            outfile['x'][data_ndx] = x_final
+            outfile['y'][data_ndx] = y_final
             outfile['scene_id'][data_ndx] = scene_id
             outfile['scan_id'][data_ndx] = scan_id 
             outfile['world_to_grid'][data_ndx] = world_to_grid
-            
-            outfile['frames'][data_ndx] = 1
+
+            # find 1 nearest image to this grid
+            outfile['frames'][data_ndx] = get_nearest_images(world_to_grid, num_nearest_imgs,
+                                                        scan_name, cfg['data']['root'],
+                                                        cfg['data']['frame_skip'],
+                                                        img_size,
+                                                        projector)
+
             data_ndx += 1
             break 
         break
 
     outfile.close()
-
-def add_translation(transform, t):
-    new_transform = transform.copy()
-    new_transform[:3, 3] += t
-    return new_transform
-
-def get_world_to_scene(voxel_size, translation):
-    t = np.eye(4, dtype=np.float32) / voxel_size
-    # insert the translation
-    t[:3, 3] = translation
-    t[3, 3] = 1
-
-    return t
-
-def get_scene_scan_ids(path):
-    '''
-    path: /mnt/data/scannet/scans/scene0673_05/scene0673_05_voxelized.ply
-    output: 673, 05 (ints)
-    '''
-    return int(path.stem[5:9]), int(path.stem[10:12])
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
