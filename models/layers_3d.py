d4ba22e217e2cb11f392370f427f1bcadae028ea
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-09-26 10:17:31 +0200

    Add relu option to all 3d layers

--- models/layers_3d.py
+++ models/layers_3d.py
@@ -15,7 +15,7 @@ class SameConv3D(nn.Module):
         return self.layers(x)
 
 class Down3D(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout=True, relu=True):
         super().__init__()
         # x->x/2 dimensions
         self.layers = nn.Sequential(
@@ -25,15 +25,15 @@ class Down3D(nn.Module):
             nn.ReLU(),
             # same
             nn.Conv3d(out_channels, out_channels, 3, 1, 1),
-            nn.ReLU(),
-            nn.Dropout3d(0.1)
+            nn.ReLU() if relu else nn.Identity(),
+            nn.Dropout3d(0.1) if dropout else nn.Identity()
         )
 
     def forward(self, x):
         return self.layers(x)
 
 class Up3D(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout=True):
+    def __init__(self, in_channels, out_channels, dropout=True, relu=True):
         super().__init__()
         self.layers = nn.Sequential(
             # inchannels, outchannels, kernel, stride, padding, output_padding
@@ -42,7 +42,7 @@ class Up3D(nn.Module):
             nn.ReLU(),
             # same
             nn.Conv3d(out_channels, out_channels, 3, 1, 1),
-            nn.ReLU(),
+            nn.ReLU() if relu else nn.Identity(),
             nn.Dropout3d(0.1) if dropout else nn.Identity()
         )
 
