3d0010ac33ca68a4d072a15a7bec40c9f4db158e
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-11-02 08:36:14 +0100

    Add big 3d layers with res connection

--- models/layers_3d.py
+++ models/layers_3d.py
@@ -14,6 +14,73 @@ class SameConv3D(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+class Down3D_Big(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout=True):
+        super().__init__()
+        # x->x/2
+        self.block1 = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, 3, 2, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+        # same conv
+        self.block2 = nn.Sequential(
+            nn.Conv3d(out_channels, out_channels, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+        # same conv
+        self.block3 = nn.Sequential(
+            nn.Conv3d(out_channels, out_channels, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+
+        self.dropout = nn.Dropout3d(0.2) if dropout else nn.Identity()
+
+    def forward(self, x):
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        # res connection
+        x3 = self.block3(x2) + x1
+        x4 = self.dropout(x3)
+
+        return x4
+
+class Up3D_Big(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout=True):
+        super().__init__()
+        # x->2x
+        self.block1 = nn.Sequential(
+            nn.ConvTranspose3d(in_channels, out_channels, 4, 2, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+        # same conv
+        self.block2 = nn.Sequential(
+            nn.Conv3d(out_channels, out_channels, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+        # same conv
+        self.block3 = nn.Sequential(
+            nn.Conv3d(out_channels, out_channels, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+        )
+
+        self.dropout = nn.Dropout3d(0.2) if dropout else nn.Identity()
+
+    def forward(self, x):
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        # res connection
+        x3 = self.block3(x2) + x1
+        x4 = self.dropout(x3)
+
+        return x4
+
+
 class Down3D(nn.Module):
     def __init__(self, in_channels, out_channels, dropout=True, relu=True):
         super().__init__()
