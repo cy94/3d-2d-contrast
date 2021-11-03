d35a2a79935bfd38daa7ee2951e0370e814266b0
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-11-03 22:34:13 +0100

    Use 2d class weights in ENet

--- models/sem_seg/enet.py
+++ models/sem_seg/enet.py
@@ -1,6 +1,7 @@
 # ENet: https://arxiv.org/abs/1606.02147
 # based off of https://github.com/bermanmaxim/Enet-PyTorch
 
+from datasets.scannet.common import CLASS_WEIGHTS_ALL_2D
 from models.sem_seg.fcn3d import SemSegNet
 import torch
 import torch.nn as nn
@@ -1175,6 +1176,19 @@ class ENet2(SemSegNet):
             stride=2,
             padding=1,
             bias=False)
+
+    def init_class_weights(self, cfg):
+        if cfg['train']['class_weights']:
+            print('Using class weights for 2D model')
+            weights = {
+                40: CLASS_WEIGHTS_ALL_2D
+            }
+            if self.num_classes in weights:
+                self.class_weights = torch.Tensor(weights[self.num_classes])
+            else:
+                raise NotImplementedError(f'Add class weights for {self.num_classes} classes')
+        else: 
+            self.class_weights = None
 
     def forward(self, x, return_features=False):
         # Initial block
