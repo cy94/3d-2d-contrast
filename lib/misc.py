da81a928e4746ba53df7b29e60f37f0c4e84a04b
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2021-11-16 21:36:28 +0100

    Fix early stopping with contrastive loss

--- lib/misc.py
+++ lib/misc.py
@@ -122,7 +122,7 @@ def get_logger_and_callbacks(args, cfg):
         print(f'Add early stopping callback on {monitor_key}')
         callbacks.append(
             # loss ~ 3, need to improve atleast 0.01
-            EarlyStopping(monitor="loss/val", min_delta=0.005, 
+            EarlyStopping(monitor=monitor_key, min_delta=0.005, 
             patience=5, verbose=True, mode="min", strict=True,
             check_finite=True,)
         )
